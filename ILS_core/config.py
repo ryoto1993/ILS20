1aa9a0923c57cad68043ce7858de93660eb34209
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2016-08-01 09:45:10 +0900

    change directory hierarchy

---
+++ ILS_core/config.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+
+class Config:
+    def __init__(self):
+        ####################
+        #     基本設定     #
+        ####################
+        self.SENSOR_FILE = ""
+        self.LIGHT_FILE = ""
--- ILS_core/equipment/Light.py
+++ ILS_core/equipment/Light.py
@@ -2,12 +2,14 @@
 
 
 class Light:
+    u"""照明のオブジェクト"""
     def __init__(self):
         # 照明設置位置座標
         self.posX = 0.0
         self.posY = 0.0
         self.posZ = 0.0
-
         # 照明属性
         self.minLum = 0.0  # 最小点灯光度
         self.maxLum = 0.0  # 最大点灯光度
+        # ステータス
+        self.isOn = True  # 照明のスイッチ
