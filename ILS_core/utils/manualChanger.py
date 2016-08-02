6f1b958086363a5a7755ac031ccda2b5a5b349dc
Hiroaki Nasu <bun0048@mail4.doshisha.ac.jp>
2016-08-02 15:34:25 +0900

    add comments to manualChanger.py

--- ILS_core/utils/manualChanger.py
+++ ILS_core/utils/manualChanger.py
@@ -5,6 +5,7 @@ import time
 
 
 def change_to_max(lights):
+    u"""すべての照明の信号値をLIGHT_SIGNAL_MAXに切り替えるメソッド"""
     for l in lights:
         # 色温度制御までするとき
         if INIT.TEMPERATURE:
@@ -17,6 +18,7 @@ def change_to_max(lights):
 
 
 def change_to_min(lights):
+    u"""すべての照明の信号値をLIGHT_SIGNAL_MINに切り替えるメソッド"""
     for l in lights:
         # 色温度制御までするとき
         if INIT.TEMPERATURE:
@@ -29,14 +31,20 @@ def change_to_min(lights):
 
 
 def change_to_fixed_pattern(lights):
+    u"""LIGHT_PATTERN_FILEで定義した点灯パターンの信号値に切り替えるメソッド"""
     pattern = light_pattern_reader()
 
+    if not len(pattern[0]) == len(lights[0].signals):
+        print("Error, pattern config file isn't match to number of light signals.")
+        return
     for i, l in enumerate(lights):
         for j in range(len(l.signals)):
             l.signals[j] = pattern[i][j]
     time.sleep(0.1)
 
+
 def change_to_zero(lights):
+    u"""すべての照明の信号値を0に切り替えるメソッド"""
     for l in lights:
         for i in range(len(l.signals)):
             l.signals[i] = 0
