297791b7dcb3f576c9d05b9f4613c1756149e7b4
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2016-09-18 15:49:41 +0900

    delete miss algorithm

--- ILS_toolkit/algorithm/algorithmCommon.py
+++ ILS_toolkit/algorithm/algorithmCommon.py
@@ -50,68 +50,6 @@ def calc_objective_function_influence(ils, next_flag):
             l.objective_penalty = penalty
 
 
-def decide_next_luminosity_influence(ils):
-    u"""
-    全照明の次光度を照度/光度影響度から決定するメソッド
-
-    ★次光度決定方法のポリシー★
-    各照明がINIT.ALG_DB_THRESHOLD以上のセンサの中から
-    INIT.ALG_DB_CHECK_SENSOR_NUM個分のセンサの現在照度と目標照度をチェックし，
-    増光・中立・減光から近傍設計を選択する．
-    優先度は，減光→増光→中立
-
-    :param ils: ILSを引数に渡す
-    """
-
-    # 各照明ごとに次光度を決定
-    for l in ils.lights:
-        neighbor = NeighborType.dimming
-
-        # センサに対する照度/光度影響度を降順にした時のインデックス番号
-        influence = l.influence[:]
-
-        # 離席してるセンサの影響度は0にする
-        for s_i, s in enumerate(ils.sensors):
-            if not s.attendance:
-                influence[s_i] = 0.0
-
-        key = sorted(range(len(influence)), key=lambda k: influence[k], reverse=True)
-
-        # INIT.ALG_DB_CHECK_SENSOR_NUMの数だけ近くのセンサを抽出
-        for i in range(0, INIT.ALG_DB_CHECK_SENSOR_NUM):
-            s = ils.sensors[key[i]]
-            if not s.attendance:
-                continue
-            # 増光対象があるかチェック
-            if s.illuminance < s.target * (100.0 + INIT.ALG_ALLOWANCE_LOWER) / 100.0:
-                neighbor = NeighborType.brightening
-                break
-            # 中立対象があるかチェック
-            if s.illuminance < s.target + INIT.ALG_ALLOWANCE_UPPER:
-                neighbor = NeighborType.neutral
-        l.neighbor = neighbor
-
-        # 次光度をneighborから決定
-        change_rate = 0
-        if neighbor == NeighborType.brightening:
-            change_rate = random.randint(INIT.ALG_DB_BRIGHTENING_LOWER, INIT.ALG_DB_BRIGHTENING_UPPER)
-        elif neighbor == NeighborType.neutral:
-            change_rate = random.randint(INIT.ALG_DB_NEUTRAL_LOWER, INIT.ALG_DB_NEUTRAL_UPPER)
-        elif neighbor == NeighborType.dimming:
-            change_rate = random.randint(INIT.ALG_DB_DIMMING_LOWER, INIT.ALG_DB_DIMMING_UPPER)
-
-        l.previous_luminosity = l.luminosity
-        l.next_luminosity = l.luminosity * (100.0 + change_rate) / 100.
-        if l.next_luminosity > INIT.LIGHT_LUMINOSITY_MAX[0]:
-            l.next_luminosity = INIT.LIGHT_LUMINOSITY_MAX[0]
-        elif l.next_luminosity < INIT.LIGHT_LUMINOSITY_MIN[0]:
-            l.next_luminosity = INIT.LIGHT_LUMINOSITY_MIN[0]
-
-    for l in ils.lights:
-        l.luminosity = l.next_luminosity
-        convert_to_signal(ils.lights)
-
-
 def decide_next_luminosity(ils):
     # 各照明ごとに次光度を決定
     for l in ils.lights:
