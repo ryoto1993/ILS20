bf182bb6ceb3d85d69299ce60340643a51b9e4b5
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2016-10-16 15:26:37 +0900

    update signalconverter.py

--- ILS_toolkit/utils/signalConverter.py
+++ ILS_toolkit/utils/signalConverter.py
@@ -1,6 +1,6 @@
 ﻿# coding: utf-8
 
-from configure.config import *
+from configure.config import INIT
 
 
 def convert_to_luminosity(lights):
@@ -8,7 +8,9 @@ def convert_to_luminosity(lights):
 
     # TODO: 現在は1つめの信号値のみだが，色温度制御の時にはここを書き換える必要がある．
     for l in lights:
-        l.luminosity = int(-0.0130119460805247 * float(l.signals[0]) * float(l.signals[0]) + 13.9104144531693 * float(l.signals[0]) - 40.0024478040349)
+        l.luminosity = int(-0.0130119460805247 * float(l.signals[0]) * float(l.signals[0]) + 13.9104144531693 *
+                           float(l.signals[0]) - 40.0024478040349)
+
 
 def convert_to_signal(lights):
     u"""光度値を信号値に変換する"""
@@ -17,4 +19,5 @@ def convert_to_signal(lights):
         pass
     else:
         for l in lights:
-            l.signals[0] = int(0.00000664944924261323 * float(l.luminosity) * float(l.luminosity) + 0.0711852025585447 * float(l.luminosity) + 3.06397372065703)
+            l.signals[0] = int(0.00000664944924261323 * float(l.luminosity) * float(l.luminosity) +
+                               0.0711852025585447 * float(l.luminosity) + 3.06397372065703)
