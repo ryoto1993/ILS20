ae0522693e066319cef94410288a5125f4df7524
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2016-10-17 01:25:46 +0900

    update Power.py and DaySimulator.py

--- ILS_toolkit/equipment/PowerMeter.py
+++ ILS_toolkit/equipment/PowerMeter.py
@@ -10,18 +10,19 @@ class PowerMeter:
         self.power = 0
         self.actual_power = 0
 
-    def calc_power(self):
-        u"""
-        照明の光度値を合計するメソッド
-        消費電力は各照明の光度値の総和
-        """
-        power = 0.0
-        actual = 0.0
-        for l in self.lights:
-            power += l.luminosity
-            # y=0.4257x + 0.3716に基づいて計算
-            actual += l.signals[0] * 0.4257 + 0.3716
-        self.power = power
-        self.actual_power = actual
 
-        return int(power)
+def calc_power(self):
+    u"""
+    照明の光度値を合計するメソッド
+    消費電力は各照明の光度値の総和
+    """
+    power = 0.0
+    actual = 0.0
+    for l in self.lights:
+        power += l.luminosity
+        # y=0.4257x + 0.3716に基づいて計算
+        actual += l.signals[0] * 0.4257 + 0.3716
+    self.power = power
+    self.actual_power = actual
+
+    return int(power)
--- ILS_toolkit/experiments/DaySimulator.py
+++ ILS_toolkit/experiments/DaySimulator.py
@@ -5,15 +5,19 @@ import csv
 from configure.config import INIT
 from ILS import ILS
 from algorithm.ANArank import ANARANK
+from equipment.Sensor import Sensor
+from equipment.Light import Light
+from equipment.Sensor import update_sensors
+from equipment.PowerMeter import calc_power
 
 
 def simulate():
     # ################### #
     #       実験用設定      #
     # ################### #
-    days = 20
-    start_hr = 9
-    loop = 800
+    days = 20       # <- 何日分のシミュレーションを行うか（その分の"ptn (xx).csv"を用意してね）
+    start_hr = 9    # <- 1日の開始時刻（終了時刻は在離席ファイルの行数に依存）
+    loop = 400      # <- ステップ数ではなくループ数，400とすると800ステップになる
     path = u"./experiments/d平均在席率3パターン_名前変更/平均在席率30%"
     # ################### #
 
@@ -38,15 +42,37 @@ def simulate():
         # 毎時の処理を行う
         for h in range(start_hr, start_hr+len(att_data)):
             print("... Hour " + str(h) + " ...")
+            # シーケンスネームを設定（ログ用）
             INIT.SEQUENCE_NAME = "Day" + str(d) + "_Hour" + str(h)
-
-            ils = ILS()  # ILSを初期化
+            # センサ番号と照明番号のリセット（ログ用）
+            Sensor.id = 1
+            Light.id = 1
+            # ILS初期化
+            ils = ILS()
+            # 在離席の設定
+            set_attendance(att_data[h-start_hr], ils)
+            # アルゴリズムの設定
             ils.algorithm = ANARANK(ils)
-
-            for l in range(int(loop/2)):
+            # 指定したループ数の演算を実行
+            for l in range(loop):
                 ils.algorithm.next_step()
+            # 最終状態の電力情報とセンサ情報をアップデート
+            # 現在照度値を取得
+            update_sensors(self.ils)
+            # 電力情報を計算
+            self.ils.power_meter.calc_power()
 
     exit(0)
+
+
+def set_attendance(data, ils):
+    for s_i, s in enumerate(ils.sensors):
+        if data[s_i] == "0":
+            s.attendance = False
+        elif data[s_i] == "1":
+            s.attendance = True
+        else:
+            print("Attendance setting Error!\nPlease check your attendance setting data.")
 
 
 def force_config():
